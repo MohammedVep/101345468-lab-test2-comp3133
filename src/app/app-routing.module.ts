6d81dc3e503400ee8e6c411e512cb4254cee7a9a
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2023-04-04 19:04:15 -0400

    PE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { MissionlistComponent } from './missionlist/missionlist.component';
 import { MissiondetailsComponent } from './missiondetails/missiondetails.component';
 
 const routes: Routes = [
-   { path: '', redirectTo: '/list', pathMatch: 'full' },
-  { path: 'list', component: MissionlistComponent },
-  { path: 'detail', component: MissiondetailsComponent },
-  
+  { path: '', redirectTo: '/missions', pathMatch: 'full' },
+  { path: '', component: MissionlistComponent },
+  { path: 'missions/:flight_number', component: MissiondetailsComponent }
 ];
 
 @NgModule({
